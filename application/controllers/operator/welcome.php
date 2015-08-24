c8eed039ef057af8045165a4585508d9e70ccbb6
Fairuz <fairuz.p@students.amikom.ac.id>
2015-08-24 07:27:00 +0700

    add menu
    nambahin menu sama benerin tabel data siswa

---
+++ application/controllers/operator/welcome.php
@@ -0,0 +1,30 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+// load base class if needed
+require_once( APPPATH . 'controllers/base/OperatorBase.php' );
+
+class welcome extends ApplicationBase {
+
+    // constructor
+    public function __construct() {
+        parent::__construct();
+        // load model
+        // load library
+        $this->load->library('tnotification');
+        $this->load->library('datetimemanipulation');
+    }
+
+    // welcome administrator
+    public function index() {
+        // set page rules
+        $this->_set_page_rule("R");
+        // set template content
+        $this->smarty->assign("template_content", "operator/welcome/index.html");
+
+        // output
+        parent::display();
+    }
+
+}
